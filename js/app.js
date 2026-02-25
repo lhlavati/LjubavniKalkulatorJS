0c8514b68c2adf1f1b82cabaa03391c21ca009ec
lhlavati <lukahlavati@gmail.com>
2026-02-25 21:39:18 +0100

    fn za zbrajanje ponovljenih slova i validacija

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
 const btn = document.getElementById('calculate-btn');
 
-function containsNum(str1, str2){
-    let containsNumber = false;
+function validateStr(str){
+    let checkSpace = false;
+    let checkChar = false;
 
-    for(let i = 0; i < str1.length; i++){
-        if(!isNaN(str1[i])){
-            containsNumber = true;
+    checkChar = /^[a-zčćđšžA-ZČĆĐŠŽ]+$/.test(str) ? true : false;
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === ' '){
+            checkSpace = false;
             break;
+        } else {
+            checkSpace = true;
         }
     }
-    for(let i = 0; i < str2.length; i++){
-        if(!isNaN(str2[i])){
-            containsNumber = true;
-            break;
+
+    return checkChar && checkSpace;
+}
+
+function countLetters(str1, str2){
+    let joinedStr = str1.toLowerCase() + str2.toLowerCase();
+        
+        let arr1 = [];
+        let arr2 = [];
+        let counter1 = 0;
+        let counter2 = 0;
+
+        for(let i = 0; i < str1.length; i++){
+            for(let j = 0; j < joinedStr.length; j++){
+                if(str1[i].toLowerCase() === joinedStr[j]){
+                    counter1++;
+                }             
+            }
+            arr1.push(counter1);
+            counter1 = 0;
         }
-    }
-    return containsNumber;
+
+        for(let i = 0; i < str2.length; i++){
+            for(let j = 0; j < joinedStr.length; j++){
+                if(str2[i].toLowerCase() === joinedStr[j]){
+                    counter2++;
+                }
+            }
+            arr2.push(counter2);
+            counter2 = 0;
+        }
+        console.log(arr1);
+        console.log(arr2);
+        return [arr1, arr2];
 }
 
 btn.addEventListener('click', () => {
@@ -28,9 +60,15 @@ btn.addEventListener('click', () => {
         return;
     }
 
-    if(containsNum(name1, name2)){
-        result.innerHTML = "Jedno od polja sadrži broj u imenu!";
+    if(validateStr(name1) && validateStr(name2)){
+        result.innerHTML = "Bravo!"
+        countLetters(name1, name2)
+        return;
+    } else {
+        result.innerHTML = "Ponovi!"
         return;
     }
+
+    
 
 })
